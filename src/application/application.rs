2d1c41b5d667ff554f8b769e90452724ef2b5f24
mkazutaka <paper.sheet.kami@gmail.com>
2020-01-28 02:48:14 +0900

    move backup method to app

--- src/application/application.rs
+++ src/application/application.rs
@@ -1,9 +1,12 @@
 use crate::application::ApplicationVisitor;
+use fs_extra::dir::{move_dir, CopyOptions};
 use rust_embed::RustEmbed;
 use serde::de::{Deserializer, MapAccess, Visitor};
 use serde::export::fmt::Error;
 use serde::export::Formatter;
 use serde::Deserialize;
+use std::fs;
+use std::path::Path;
 
 #[derive(Debug, Default)]
 pub struct Application {
@@ -12,7 +15,47 @@ pub struct Application {
     pub paths: Vec<String>,
 }
 
-impl Application {}
+impl Application {
+    pub fn backup(&self, ram: &str) {
+        for path in &self.paths {
+            println!("backup: {}", path.as_str());
+            let path = shellexpand::tilde(&path).to_string();
+            let path = Path::new(path.as_str());
+
+            let dir_path = path.parent().unwrap();
+            let dir_path = dir_path.to_str().unwrap();
+            let app_path = path.to_str().unwrap();
+            let ram_dir_path = format!("/Volumes/{}{}", ram, dir_path);
+            let ram_app_path = format!("/Volumes/{}{}", ram, app_path);
+
+            let mut option = CopyOptions::new();
+            option.copy_inside = true;
+
+            fs::create_dir_all(&ram_dir_path).unwrap();
+            move_dir(&app_path, &ram_dir_path, &option).unwrap();
+            std::os::unix::fs::symlink(&ram_app_path, &app_path).unwrap();
+        }
+    }
+
+    pub fn restore(&self, ram: &str) {
+        for path in &self.paths {
+            println!("restore: {}", path.as_str());
+            let path = shellexpand::tilde(&path).to_string();
+            let path = Path::new(path.as_str());
+
+            let dir_path = path.parent().unwrap();
+            let dir_path = dir_path.to_str().unwrap();
+            let app_path = path.to_str().unwrap();
+            let _ram_dir_path = format!("/Volumes/{}{}", ram, dir_path);
+            let ram_app_path = format!("/Volumes/{}{}", ram, app_path);
+
+            fs::remove_file(&app_path).unwrap();
+            let mut option = CopyOptions::new();
+            option.copy_inside = true;
+            move_dir(&ram_app_path, &dir_path, &option).unwrap();
+        }
+    }
+}
 
 impl<'de> Deserialize<'de> for Application {
     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
--- src/ramup.rs
+++ src/ramup.rs
@@ -27,50 +27,13 @@ impl Ramup {
 
     pub fn backup(&mut self) {
         for app in &mut self.config.applications {
-            for path in &app.paths {
-                println!("backup: {}", path.as_str());
-                let path = shellexpand::tilde(&path).to_string();
-                let path = Path::new(path.as_str());
-
-                let dir_path = path.parent().unwrap();
-                let dir_path = dir_path.to_str().unwrap();
-                let app_path = path.to_str().unwrap();
-                let ram_dir_path = format!("/Volumes/{}{}", self.config.ram.name, dir_path);
-                let ram_app_path = format!("/Volumes/{}{}", self.config.ram.name, app_path);
-
-                let mut option = CopyOptions::new();
-                option.copy_inside = true;
-
-                fs::create_dir_all(&ram_dir_path).unwrap();
-                move_dir(&app_path, &ram_dir_path, &option).unwrap();
-                std::os::unix::fs::symlink(&ram_app_path, &app_path).unwrap();
-            }
+            app.backup(&self.config.ram.name);
         }
     }
 
     pub fn restore(&self) {
         for app in &self.config.applications {
-            for path in &app.paths {
-                println!("restore: {}", path.as_str());
-                let path = shellexpand::tilde(&path).to_string();
-                let path = Path::new(path.as_str());
-
-                let dir_path = path.parent().unwrap();
-                let dir_path = dir_path.to_str().unwrap();
-                let app_path = path.to_str().unwrap();
-                let _ram_dir_path = format!("/Volumes/{}{}", self.config.ram.name, dir_path);
-                let ram_app_path = format!("/Volumes/{}{}", self.config.ram.name, app_path);
-
-                fs::remove_file(&app_path).unwrap();
-                let mut option = CopyOptions::new();
-                option.copy_inside = true;
-                move_dir(&ram_app_path, &dir_path, &option).unwrap();
-
-                Command::new("hdiutil")
-                    .args(&["detach", &self.mount_point])
-                    .output()
-                    .expect("detach is failed");
-            }
+            app.restore(&self.config.ram.name);
         }
     }
 }
