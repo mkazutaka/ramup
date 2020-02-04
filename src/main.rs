5ec319d07c05a61049fa675f09c91afb725d3bd0
mkazutaka <paper.sheet.kami@gmail.com>
2020-02-04 10:34:59 +0900

    use subcommand name()

--- src/main.rs
+++ src/main.rs
@@ -13,63 +13,60 @@ use anyhow::{Context, Result};
 use clap::load_yaml;
 use clap::App;
 
-static SUB_COMMAND_INIT: &str = "init";
+//static SUB_COMMAND_INIT: &str = "init";
 static SUB_COMMAND_BACKUP: &str = "backup";
 static SUB_COMMAND_RESTORE: &str = "restore";
-static SUB_COMMAND_CLEAN: &str = "clean";
+//static SUB_COMMAND_CLEAN: &str = "clean";
 
 fn main() -> Result<()> {
     let yaml = load_yaml!("cli.yml");
     let arg_matches = App::from_yaml(yaml).get_matches();
 
     let config = cfg::Config::load()?;
-
     let state = state::State::load();
     let apps = config.applications;
     let ram = config.ram;
 
-    if arg_matches.subcommand_matches(SUB_COMMAND_INIT).is_some() {
-        return cfg::Config::initialize();
-    }
-
     let mut handler = handler::Handler::new(ram, state);
 
-    if let Some(matches) = arg_matches.subcommand_matches(SUB_COMMAND_BACKUP) {
-        let mut sources: Vec<String> = vec![];
-        if matches.is_present("path") {
-            let path = matches.value_of("path").with_context(|| "path not found")?;
-            sources.push(path.to_string());
-        } else {
-            for app in &apps {
-                for path in &app.paths {
-                    sources.push(path.clone());
+    match arg_matches.subcommand_name() {
+        Some("init") => cfg::Config::initialize()?,
+        Some("backup") => {
+            let matches = arg_matches
+                .subcommand_matches(SUB_COMMAND_BACKUP)
+                .with_context(|| "Arg not found")?;
+            let mut sources: Vec<String> = vec![];
+            if matches.is_present("path") {
+                let path = matches.value_of("path").with_context(|| "path not found")?;
+                sources.push(path.to_string());
+            } else {
+                for app in &apps {
+                    for path in &app.paths {
+                        sources.push(path.clone());
+                    }
                 }
             }
+            handler.backup(sources)?
         }
-
-        handler.backup(sources)?;
-        return Ok(());
-    }
-
-    if let Some(matches) = arg_matches.subcommand_matches(SUB_COMMAND_RESTORE) {
-        let mut sources: Vec<String> = vec![];
-        if matches.is_present("path") {
-            let path = matches.value_of("path").with_context(|| "path not found")?;
-            sources.push(path.to_string());
-        } else {
-            for app in &apps {
-                for path in &app.paths {
-                    sources.push(path.clone());
+        Some("restore") => {
+            let matches = arg_matches
+                .subcommand_matches(SUB_COMMAND_RESTORE)
+                .with_context(|| "Arg not found")?;
+            let mut sources: Vec<String> = vec![];
+            if matches.is_present("path") {
+                let path = matches.value_of("path").with_context(|| "path not found")?;
+                sources.push(path.to_string());
+            } else {
+                for app in &apps {
+                    for path in &app.paths {
+                        sources.push(path.clone());
+                    }
                 }
             }
+            handler.restore(sources)?
         }
-
-        handler.restore(sources)?;
-        return Ok(());
-    }
-
-    if arg_matches.subcommand_matches(SUB_COMMAND_CLEAN).is_some() {
-        handler.clean()?;
+        Some("clean") => handler.clean()?,
+        _ => (),
     }
 
     Ok(())
